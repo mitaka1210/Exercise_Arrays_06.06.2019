a239fd9d95ad7539271015cb48f137f6bccfcb5e
Dimitar <dimitar_dimitrov12@mail.bg>
2019-06-11 10:18:38 +0300

    Arrays_Exercise
    Arrays_Exercise

---
+++ 05. Top Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] number = Console.ReadLine()
+               .Split(" ")
+               .Select(int.Parse)
+               .ToArray();
+
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                bool isItBigger = true;
+                for (int j = i + 1; j < number.Length; j++)
+                {
+                    if (number[i] <= number[j])
+                    {
+                        isItBigger = false;
+                    }
+                }
+                if (isItBigger)
+                {
+                   Console.Write(number[i] + " ");
+                }
+               
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 06.Top_Integers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] n = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] > number)
+                {
+                    number = n[i];
+                }
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
